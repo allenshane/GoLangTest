cbf4dda64d9198ef7321fdf679053a508d1b3336
Allen Shane <>
2020-02-11 18:14:37 +0530

    Added for loops in functions to work with multiple folders and databases. Added messages as feedback

--- src/test/test.go
+++ src/test/test.go
@@ -11,18 +11,20 @@ import (
 )
 
 type Config struct {
-	Folders struct {
-		Source      string `json:"source"`
-		Destination string `json:"destination"`
-	} `json:"folders"`
+	DirecoriesToCopy struct {
+		Source      []string `json:"source"`
+		Destination string   `json:"destination"`
+	} `json:"directoriestocopy"`
 	Mysqldb struct {
-		Username string   `json:"username"`
-		Password string   `json:"password"`
-		Dbs      []string `json:"dbs"`
+		Username    string   `json:"username"`
+		Password    string   `json:"password"`
+		Dbs         []string `json:"dbs"`
+		Destination string   `json:"destination"`
 	} `json:"mysqldb"`
 }
 
 func LoadConfiguration(file string) (config Config, err error) {
+	fmt.Println("Loading Config....")
 	configFile, err := os.Open(file)
 	defer configFile.Close()
 	if err != nil {
@@ -33,8 +35,19 @@ func LoadConfiguration(file string) (config Config, err error) {
 	return
 }
 
-func MysqlDump(username string, password string, dbs []string) {
+func CopyDirectories(sources []string, destination string) {
+	fmt.Println("Copying Directories....")
+	for i, source := range sources {
+		fmt.Printf("Copying [%d] ==> [%s] to [%s]\n", i, source, destination)
+		copy.CopyDirectory(source, destination)
 
+	}
+	fmt.Println("Copying Completed!!!")
+}
+
+func MysqlDump(username string, password string, dbs []string, destination string) {
+	os.Mkdir(destination, 0700)
+	fmt.Printf("Starting MySQL Dump")
 	for i, db := range dbs {
 		fmt.Printf("Dumping [%d] ==> [%s]\n", i, db)
 		cmd := exec.Command("mysqldump", "-P3306", "-h127.0.0.1", "-uroot", "-ppassword", db)
@@ -51,16 +64,18 @@ func MysqlDump(username string, password string, dbs []string) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = ioutil.WriteFile("./out.sql", bytes, 0644)
+		result := destination + "/" + db + ".sql"
+		err = ioutil.WriteFile(result, bytes, 0644)
 		if err != nil {
 			panic(err)
 		}
 	}
+	fmt.Printf("MySQL Dump Completed")
 }
 
 func main() {
 	fmt.Println("Starting the app....")
 	config, _ := LoadConfiguration("E:/go/config.json")
-	copy.CopyDirectory(config.Folders.Source, config.Folders.Destination)
-	MysqlDump(config.Mysqldb.Username, config.Mysqldb.Password, config.Mysqldb.Dbs)
+	CopyDirectories(config.DirecoriesToCopy.Source, config.DirecoriesToCopy.Destination)
+	MysqlDump(config.Mysqldb.Username, config.Mysqldb.Password, config.Mysqldb.Dbs, config.Mysqldb.Destination)
 }
