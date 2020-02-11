100f8a4b4d130823659af383825348715217f935
Allen Shane <>
2020-02-11 17:45:05 +0530

    Added struct to get db data from config file

--- src/test/test.go
+++ src/test/test.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github/copy"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 )
 
 type Config struct {
@@ -12,6 +15,11 @@ type Config struct {
 		Source      string `json:"source"`
 		Destination string `json:"destination"`
 	} `json:"folders"`
+	Mysqldb struct {
+		Username string   `json:"username"`
+		Password string   `json:"password"`
+		Dbs      []string `json:"dbs"`
+	} `json:"mysqldb"`
 }
 
 func LoadConfiguration(file string) (config Config, err error) {
@@ -25,8 +33,34 @@ func LoadConfiguration(file string) (config Config, err error) {
 	return
 }
 
+func MysqlDump(username string, password string, dbs []string) {
+
+	for i, db := range dbs {
+		fmt.Printf("Dumping [%d] ==> [%s]\n", i, db)
+		cmd := exec.Command("mysqldump", "-P3306", "-h127.0.0.1", "-uroot", "-ppassword", db)
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		bytes, err := ioutil.ReadAll(stdout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile("./out.sql", bytes, 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Starting the app....")
-	config, _ := LoadConfiguration(os.Args[0])
+	config, _ := LoadConfiguration("E:/go/config.json")
 	copy.CopyDirectory(config.Folders.Source, config.Folders.Destination)
+	MysqlDump(config.Mysqldb.Username, config.Mysqldb.Password, config.Mysqldb.Dbs)
 }
