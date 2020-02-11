3bd5c9fa289c01f2aa305a7ddb76b7a77987836f
Allen Shane <>
2020-02-11 19:22:30 +0530

    Added function to compress target directory and add a test json file

--- src/test/test.go
+++ src/test/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
 	"github/copy"
@@ -21,6 +22,10 @@ type Config struct {
 		Dbs         []string `json:"dbs"`
 		Destination string   `json:"destination"`
 	} `json:"mysqldb"`
+	Compression struct {
+		Source      string `json:"source"`
+		Destination string `json:"destination"`
+	} `json:"compression"`
 }
 
 func LoadConfiguration(file string) (config Config, err error) {
@@ -50,7 +55,7 @@ func MysqlDump(username string, password string, dbs []string, destination strin
 	fmt.Printf("Starting MySQL Dump")
 	for i, db := range dbs {
 		fmt.Printf("Dumping [%d] ==> [%s]\n", i, db)
-		cmd := exec.Command("mysqldump", "-P3306", "-h127.0.0.1", "-uroot", "-ppassword", db)
+		cmd := exec.Command("mysqldump", "-u"+username, "-p"+password, db)
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
 			log.Fatal(err)
@@ -73,9 +78,75 @@ func MysqlDump(username string, password string, dbs []string, destination strin
 	fmt.Printf("MySQL Dump Completed")
 }
 
+func ZipWriter(source string, destination string) {
+	fmt.Println("Starting Compression...")
+	baseFolder := source
+
+	// Get a Buffer to Write To
+	outFile, err := os.Create(destination)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer outFile.Close()
+
+	// Create a new zip archive.
+	w := zip.NewWriter(outFile)
+
+	// Add some files to the archive.
+	addFiles(w, baseFolder, "")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Make sure to check the error on Close.
+	err = w.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Compression Completed!!!")
+}
+
+func addFiles(w *zip.Writer, basePath, baseInZip string) {
+	// Open the Directory
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(basePath + file.Name())
+		if !file.IsDir() {
+			dat, err := ioutil.ReadFile(basePath + file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// Add some files to the archive.
+			f, err := w.Create(baseInZip + file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+			_, err = f.Write(dat)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if file.IsDir() {
+
+			// Recurse
+			newBase := basePath + file.Name() + "/"
+			fmt.Println("Recursing and Adding SubDir: " + file.Name())
+			fmt.Println("Recursing and Adding SubDir: " + newBase)
+
+			addFiles(w, newBase, baseInZip+file.Name()+"/")
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Starting the app....")
 	config, _ := LoadConfiguration("E:/go/config.json")
 	CopyDirectories(config.DirecoriesToCopy.Source, config.DirecoriesToCopy.Destination)
 	MysqlDump(config.Mysqldb.Username, config.Mysqldb.Password, config.Mysqldb.Dbs, config.Mysqldb.Destination)
+	ZipWriter(config.Compression.Source, config.Compression.Destination)
 }
